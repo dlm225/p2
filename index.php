38264a6a8e0eafe5821355011c4ded8ebddb1e7e
David Morgan <dlm225@g.harvard.edu>
2016-02-14 14:28:05 -0500

    Implemented Bootstrap and setup framework

--- index.php
+++ index.php
@@ -1,6 +1,60 @@
 <?php
-error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-ini_set('display_errors', 1); # Display errors on page (instead of a log file)
+  error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
+  ini_set('display_errors', 1); # Display errors on page (instead of a log file)
 ?>
 
-This is sample text.
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 3, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+<?php include 'header.php'; ?>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-4">
+			<h3 class="text-center">
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+		</div>
+		<div class="col-md-4">
+			<h3>
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+		</div>
+		<div class="col-md-4">
+			<h3>
+				h3. Lorem ipsum dolor sit amet.
+			</h3>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+		</div>
+	</div>
+</div>
+
+<?php include 'footer.php'; ?>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
