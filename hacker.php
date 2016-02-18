63bf6aa0b2efea4ce9e09a56d0f6a47a46c17928
David Morgan <dlm225@g.harvard.edu>
2016-02-17 21:05:19 -0500

    fixed validation errors

--- hacker.php
+++ hacker.php
@@ -9,7 +9,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        #page loads for 3 seconds, then RickRoll's user
+        <!-- page loads for 3 seconds, then RickRoll's user -->
         <meta http-equiv="Refresh" content="3; url=https://www.youtube.com/watch?v=dQw4w9WgXcQ">
 
         <title>High-Entropy Password Generator</title>
@@ -20,7 +20,7 @@
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
     </head>
-
+<body>
     <?php $current='hacker'; include "header.php" ?>
 
     <div class="container-fluid">
@@ -33,3 +33,5 @@
         <br />
         <h3>Redirecting...</h3>
     </div>
+</body>
+</html>
